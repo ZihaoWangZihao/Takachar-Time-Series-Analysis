f19f6518d3b0c7cefb4aa12b980698b9d2ea876d
Zihao Wang <zihaowangzihao@gmail.com>
2023-07-12 17:51:55 -0700

    See code in between ############

--- SubPlots.py
+++ SubPlots.py
@@ -12,17 +12,22 @@ col_letters = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, '
 # Columns in Excel
 fan_cols = ["C", "D", "E", "F", "G", "H"]
 temp_cols = ["I", "J", "K", "L", "M",]
-
-# Data
-x = df.iloc[:, col_letters["B"]] # Time
+output_weight_col = "R"
+input_weight_col = "V"
 auger_motor = "S"
 auger_torque = "T"
 auger_current = "U"
 auger_power = "P"
+
+
+# Data
+x = df.iloc[:, col_letters["B"]]  # Time
 y_motor = df.iloc[:, col_letters[auger_motor]]
 y_torque = df.iloc[:, col_letters[auger_torque]]
 y_current = df.iloc[:, col_letters[auger_current]]
 y_power = df.iloc[:, col_letters[auger_power]]
+y_input = df.iloc[:, col_letters[input_weight_col]]
+y_output = df.iloc[:, col_letters[output_weight_col]]
 fan_data = []
 temp_data = []
 for col in fan_cols:
@@ -55,50 +60,37 @@ ax2.legend()
 
 # # Plotting Auger
 ax3 = fig.add_subplot(2, 2, 3)
-# ax3.plot(x, y_motor, label=f"Auger Motor")
-# ax3.plot(x, y_power, label=f"Auger Power")
-# ax3.plot(x, y_current, label=f"Auger Current")
-# ax3.plot(x, y_torque, label=f"Auger Torque")
-# ax3.set_title('Auger Plots')
-# ax3.set_xlabel('Time')
-# ax3.set_ylabel('Temperature')
-# ax3.legend()
-
-# Plot the first dataset on the primary y-axis
 ax3.plot(x, y_motor, label="Auger Motor", color='tab:blue')
 ax3.plot(x, y_power, label="Auger Power", color='tab:orange')
 ax3.set_xlabel('Time')
 ax3.set_ylabel('Label 1')
 ax3.tick_params(axis='y')
 
-# Create a twin axis sharing the x-axis
-ax4 = ax3.twinx()
+ax4 = ax3.twinx() # Create a twin axis sharing the x-axis
 
-# Plot the second dataset on the secondary y-axis
 ax4.plot(x, y_current, label="Auger Current", color='tab:red')
 ax4.plot(x, y_torque, label="Auger Torque", color='tab:green')
 ax4.set_ylabel('Label 2')
 ax4.tick_params(axis='y')
 
-# Combine the legends of both axes
 lines1, labels1 = ax3.get_legend_handles_labels()
 lines2, labels2 = ax4.get_legend_handles_labels()
 lines = lines1 + lines2
 labels = labels1 + labels2
 ax3.legend(lines, labels)
 
+# Plotting Weights
+ax5 = fig.add_subplot(2, 2, 4)
+ax5.plot(x, y_output, label="Output Weight", color='tab:green')
+ax5.plot(x, y_input, label="Input Current", color='tab:red')
+ax5.set_title('Output/Input Weight Plots')
+ax5.set_xlabel('Time')
+ax5.set_ylabel('Weight')
+ax5.legend()
 
 # Adjust the spacing between subplots
 fig.tight_layout()
 
 # Display the figure
 plt.show()
-
-
-# # Add the fourth subplot
-# ax4 = fig.add_subplot(2, 2, 4)
-# ax4.plot(x, y2, label='Cos(x)')
-# ax4.plot(x, y4, label='Sqrt(x)')
-# ax4.set_title('Plot 4')
-# ax4.legend()
 
--- for_alice.py
+++ for_alice.py
@@ -2,17 +2,30 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 
-second_file_path = ""
+second_file_path = "/Users/zihaowang/PycharmProjects/Time-Series-Analysis/Takachar-Time-Series-Analysis/Hot_Test_15/Feeding Rate 22 nd June 2023.xlsx"
 df2 = pd.read_excel(second_file_path)
 
 # Iterate through the rows and columns of the DataFrame
+# for index, row in df2.iterrows():
+#     for col in df2.columns:
+#         print(col)
+#         cell_value = row[col]
+#         if cell_value == "Timestamp (hh:mm format)":
+#             # Print the row, column, and cell coordinates
+#             target_row = index + 1
+#             target_col = int(col[-1])
+#             break
+
+
+########################################################################################################################
+# Iterate through the rows and column indices of the DataFrame
 for index, row in df2.iterrows():
-    for col in df2.columns:
-        cell_value = row[col]
-        if cell_value == "Cumulative kg consumed":
+    for col_index, col in enumerate(df2.columns):
+        cell_value = row[col_index]
+        if cell_value == "Timestamp (hh:mm format)":
             # Print the row, column, and cell coordinates
             target_row = index + 1
-            target_col = int(col[-1])
+            target_col = col_index
             break
 
 # Specify the column and the starting row
@@ -21,20 +34,31 @@ starting_row = target_row
 
 # All values under "Cumulative kg consumed" column in a list
 values = df2.iloc[starting_row:, column_index].tolist()
+new_times = []
 
-# Print the values
-print(values)
+for i in values:
+    try:
+        formatted_time = i.strftime("%H:%M")
+        new_times.append(formatted_time)
+    except:
+        continue
 
-def time_difference(time1, time2):
-    # Define the two time points
-    time1 = datetime.strptime('10:30:00', '%H:%M:%S')
-    time2 = datetime.strptime('14:45:30', '%H:%M:%S')
+print(new_times)
 
-    # Calculate the time difference
-    time_diff = time2 - time1
+###############################################################################################################################
 
-    # Format the time difference in HH:MM:SS format
-    time_diff_formatted = str(time_diff)
 
-    # Print the formatted time difference
-    return time_diff_formatted
+#
+# def time_difference(time1, time2):
+#     # Define the two time points
+#     time1 = datetime.strptime('10:30:00', '%H:%M:%S')
+#     time2 = datetime.strptime('14:45:30', '%H:%M:%S')
+#
+#     # Calculate the time difference
+#     time_diff = time2 - time1
+#
+#     # Format the time difference in HH:MM:SS format
+#     time_diff_formatted = str(time_diff)
+#
+#     # Print the formatted time difference
+#     return time_diff_formatted
